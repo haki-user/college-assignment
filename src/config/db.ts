0ff8485bd98913addfff4694fe281cbe20244b06
haki-user <adityapratapsingh33@outlook.com>
2024-11-14 19:39:47 +0530

    modified:   src/config/db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (connString: string) => {
   try {
-    await mongoose.connect("mongodb://localhost/ecommerce");
+    await mongoose.connect(connString);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
